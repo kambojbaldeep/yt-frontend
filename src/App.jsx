af17fe953fdd30df6a4e748461cee8b9a6a07be5
Baldeep <kambojbaldeep@gmail.com>
2025-09-01 16:24:46 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/navbar/navbar'
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       
     </div>
   )
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import './Navbar.css'
-const navbar = () => {
+import { assets } from '../../assets/assets'
+
+
+const Navbar = () => {
   return (
     <div className='navbar'>
 
-        
+    <img src={assets.logo} alt="" className="logo" />
+
+    <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+    </ul>
+
+    <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot">
+
+            </div>
+            <button>sign in</button>
+            
+        </div>
+    </div>
+
+
       
     </div>
   )
 }
 
-export default navbar
+export default Navbar
