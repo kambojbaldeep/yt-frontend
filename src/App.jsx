f4ce03dd5cd2ff80379fb680800a7d01cce5a838
Baldeep <kambojbaldeep@gmail.com>
2025-09-02 15:57:09 +0530

    Create website header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar/>
-      
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
+
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
             <img src={assets.logo} alt="" className="logo" />
 
             <ul className="navbar-menu">
-                <li onClick={()=>setMenu("home")}className={menu=="home"?"active":""}>home</li>
-                <li onClick={()=>setMenu("menu")}className={menu=="menu"?"active":""}>menu</li>
-                <li onClick={()=>setMenu("mobile-app")}className={menu=="mobile-app"?"active":""}>mobile-app</li>
-                <li onClick={()=>setMenu("contact-us")}className={menu=="contact-us"?"active":""}>contact us</li>
+                <li onClick={() => setMenu("home")} className={menu == "home" ? "active" : ""}>home</li>
+                <li onClick={() => setMenu("menu")} className={menu == "menu" ? "active" : ""}>menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu == "mobile-app" ? "active" : ""}>mobile-app</li>
+                <li onClick={() => setMenu("contact-us")} className={menu == "contact-us" ? "active" : ""}>contact us</li>
             </ul>
 
             <div className="navbar-right">
